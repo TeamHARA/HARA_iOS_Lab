40b4b4f22ba4b667d450c1456072453769ac6b04
daminoworld <ekadls757@gmail.com>
2023-04-02 14:31:30 +0900

    Add: Groups for Members

---
+++ Hara_Demo/Hara_Demo/Damin/DaminRootViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DaminRootViewController.swift
+//  Hara_Demo
+//
+//  Created by 김담인 on 2023/04/02.
+//
+
+import UIKit
+
+class DaminRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
