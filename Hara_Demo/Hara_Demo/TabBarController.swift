b11f0db91b2d5a12777dd10e8ac0c2d330a5fcae
oy6uns <bacho4235@gmail.com>
2023-07-03 22:53:33 +0900

    [Feat] CustomTabBar 구현

--- Hara_Demo/Hara_Demo/TabBarController.swift
+++ Hara_Demo/Hara_Demo/TabBarController.swift
@@ -8,18 +8,19 @@
 import UIKit
 import SnapKit
 import Then
+import SwiftyColor
 
 final class TabBarController: UITabBarController {
     
-    let HEIGHT_TAB_BAR : CGFloat = 100
+    let HEIGHT_TAB_BAR : CGFloat = 96
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setTabBar()
-        setTabBarItemStyle()
-        setTabBarHeight()
-        self.selectedIndex = 2
+        setTabBarStyle()
+        self.selectedIndex = 0
+        self.delegate = self
     }
     
     // TabBar높이 지정해주는 메서드
@@ -29,92 +30,65 @@ final class TabBarController: UITabBarController {
         tabFrame.size.height = HEIGHT_TAB_BAR
         tabFrame.origin.y = self.view.frame.size.height - HEIGHT_TAB_BAR
         self.tabBar.frame = tabFrame
+        
+        // additionalSafeAreaInsets 설정
+        let tabBarHeightDifference = HEIGHT_TAB_BAR - 48 // 49는 기본 탭바 높이
+        if tabBarHeightDifference > 0 {
+            self.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: -tabBarHeightDifference, right: 0)
+        }
     }
     
     // TabBarItem 생성해 주는 메서드
-    private func makeTabVC(vc: UIViewController, tabBarTitle: String, tabBarImg: String, tabBarSelectedImg: String) -> UIViewController {
+    private func makeTabVC(vc: UIViewController, tabBarImg: String, tabBarSelectedImg: String) -> UIViewController {
         
-        vc.tabBarItem = UITabBarItem(title: tabBarTitle,
-                                     image: UIImage(named: tabBarImg)?.withRenderingMode(.alwaysOriginal),
+        vc.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: tabBarImg)?.withRenderingMode(.alwaysOriginal),
                                      selectedImage: UIImage(named: tabBarSelectedImg)?.withRenderingMode(.alwaysOriginal))
-        vc.tabBarItem.imageInsets = UIEdgeInsets(top: 2, left: 0, bottom: -2, right: 0)
         return vc
     }
     
-    /// TabBar의 height을 설정하는 메서드
-    private func setTabBarHeight() {
-        let height = self.view.safeAreaInsets.bottom
-        
-        var tabFrame = self.tabBar.frame
-        tabFrame.size.height = height - 150 // 원하는 높이(기존 높이 + 50)
-        tabFrame.origin.y = self.view.frame.size.height - height
-        
-        self.tabBar.frame = tabFrame
-        self.tabBar.setNeedsLayout()
-        self.tabBar.layoutIfNeeded()
-    }
-    
     /// TabBarItem을 지정하는 메서드
-    private func setTabBar() {
-        let homeTab = makeTabVC(vc: BaseNC(rootViewController: StyleVC()), tabBarTitle: "스타일", tabBarImg:"cloth", tabBarSelectedImg: "cloth_filled")
+    func setTabBar() {
+        let homeTab = makeTabVC(vc: HomeVC(), tabBarImg:"icn_home_off", tabBarSelectedImg: "icn_home_on")
         homeTab.tabBarItem.tag = 0
         
-        let categoryTab = makeTabVC(vc: BaseNC(rootViewController: AvatarVC()), tabBarTitle: "아바타", tabBarImg: "avatar", tabBarSelectedImg: "avatar_filled")
-        categoryTab.tabBarItem.tag = 1
+        let writeTab = makeTabVC(vc: WriteVC(), tabBarImg: "icn_write", tabBarSelectedImg: "avatar_filled")
+        writeTab.tabBarItem.tag = 1
         
-        let libraryTab = makeTabVC(vc: BaseNC(rootViewController: HomeVC()), tabBarTitle: "홈", tabBarImg: "home", tabBarSelectedImg: "home_filled")
-        libraryTab.tabBarItem.tag = 2
+        let storageTab = makeTabVC(vc: StorageVC(), tabBarImg: "icn_storage_off", tabBarSelectedImg: "icn_storage_on")
+        storageTab.tabBarItem.tag = 2
         
-        let notificationTab = makeTabVC(vc: BaseNC(rootViewController: MineVC()), tabBarTitle: "찜", tabBarImg: "heart", tabBarSelectedImg: "heart_filled")
-        notificationTab.tabBarItem.tag = 3
+        let insetAmount: CGFloat = 32
+        homeTab.tabBarItem.imageInsets = UIEdgeInsets(top: -4, left: insetAmount, bottom: 4, right: -insetAmount)
+        writeTab.tabBarItem.imageInsets = UIEdgeInsets(top: -4, left: 0, bottom: 4, right: 0)
+        storageTab.tabBarItem.imageInsets = UIEdgeInsets(top: -4, left: -insetAmount, bottom: 4, right: insetAmount)
         
-        let myPageTab = makeTabVC(vc: BaseNC(rootViewController: CameraVC()), tabBarTitle: "측정", tabBarImg: "camera", tabBarSelectedImg: "camera_filled")
-        myPageTab.tabBarItem.tag = 4
-        
-        let tabs = [homeTab, categoryTab, libraryTab, notificationTab, myPageTab]
+        let tabs = [homeTab, writeTab, storageTab]
         self.setViewControllers(tabs, animated: false)
     }
     
-    private func setTabBarItemStyle() {
-        UITabBar.clearShadow()
-        tabBar.layer.applyShadow(color: .gray, alpha: 0.3, x: 0, y: 0, blur: 12)
+    /// TabBar의 Style을 지정하는 메서드
+    private func setTabBarStyle() {
+        tabBar.backgroundColor = 0x2A2E33.color
+        tabBar.isTranslucent = false
         
-        // 기본적인 tabBar의 글자색을 지정해준다.
-        self.tabBar.tintColor = .black
-        
-        let tabBarAppearance = UITabBarAppearance()
-        let tabBarItemAppearance = UITabBarItemAppearance()
-        
-        tabBarItemAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemGray]
-        
-        tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
-        tabBar.standardAppearance = tabBarAppearance
-    }
-    
-}
-
-extension CALayer {
-    // Sketch 스타일의 그림자를 생성하는 유틸리티 함수
-    func applyShadow(
-        color: UIColor = .black,
-        alpha: Float = 0.5,
-        x: CGFloat = 0,
-        y: CGFloat = 2,
-        blur: CGFloat = 4
-    ) {
-        shadowColor = color.cgColor
-        shadowOpacity = alpha
-        shadowOffset = CGSize(width: x, height: y)
-        shadowRadius = blur / 2.0
+        // 탭바의 테두리를 설정합니다.
+        tabBar.layer.borderWidth = 1
+        tabBar.layer.borderColor = 0x30363D.color.cgColor
+        tabBar.layer.cornerRadius = 30
     }
 }
 
-extension UITabBar {
-    // 기본 그림자 스타일을 초기화해야 커스텀 스타일을 적용할 수 있음
-    static func clearShadow() {
-        UITabBar.appearance().shadowImage = UIImage()
-        UITabBar.appearance().backgroundImage = UIImage()
-        UITabBar.appearance().backgroundColor = UIColor.white
+// MARK: - UITabBarControllerDelegate
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 1 {
+            let writeVC = WriteVC()
+            writeVC.modalPresentationStyle = .fullScreen
+            writeVC.modalTransitionStyle = .coverVertical
+            self.present(writeVC, animated: true, completion: nil)
+            return false // 탭 변경을 막음
+        }
+        return true // 다른 탭은 정상적으로 변경
     }
 }
 
